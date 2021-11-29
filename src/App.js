101553c04b7928c2eea49f45ecd56a982902c03f
Carlos <cajavi89@gmail.com>
2021-11-29 17:36:51 -0500

    reto Incrementar, usando useState

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+const [valor,setValor] = useState(0)
+const incrementar=()=>{
+  setValor(valor+1)
+}
   return (
     <div className="App">
-      <h1>Mi primera aplicación</h1>
-      <p>Esta es mi primera aplicación en React, está padrísimo!</p>
+      <h1>{valor}</h1>
+      <button onClick={incrementar}>Incrementa</button>
     </div>
   );
 }
